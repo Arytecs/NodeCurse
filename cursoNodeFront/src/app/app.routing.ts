ed85086f66db998ede1d4d8d081c2e3605c60c5f
Ary <araceliteruel@gmail.com>
2018-09-10 15:21:21 +0200

    commit

--- cursoNodeFront/src/app/app.routing.ts
+++ cursoNodeFront/src/app/app.routing.ts
@@ -2,11 +2,13 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'registro', component: RegisterComponent }
+    { path: 'registro', component: RegisterComponent },
+    { path: 'home', component: HomeComponent }
 ];
 
 export const appRoutingProviders: any[] = [];
